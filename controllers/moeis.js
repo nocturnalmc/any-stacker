bc763fe86eee809963f890eb7865f18cbe065f71
noct <ezy_95@outlook.com>
2023-03-28 07:39:52 +0800

    feat: add MOEIS controller for sekolah & pelajar

--- controllers/moeis.js
+++ controllers/moeis.js
@@ -1,11 +1,32 @@
+import sekolahMOEIS from '../data/sekolah.json' assert { type: 'json' };
+import pelajarMOEIS from '../data/pelajar.json' assert { type: 'json' };
+
 const getSekolahMOEIS = async (req, res) => {
-  // do sekolah
-  res.status(200).json({ msg: 'get sekolah good' });
+  const { jkod, katkod } = req.query;
+
+  const sekolahByNegeri = sekolahMOEIS.sekolah_by_negeri.filter((e) => {
+    return e.NEGERI === jkod;
+  });
+
+  if (sekolahByNegeri.length <= 0) {
+    return res.status(404).json({ msg: 'No sekolah found' });
+  }
+
+  res.status(200).json(sekolahByNegeri);
 };
 
 const getPelajarMOEIS = async (req, res) => {
-  // do pelajar
-  res.status(200).json({ msg: 'get pelajar good' });
+  const { inid, pkid } = req.query;
+
+  const pelajarByKelas = pelajarMOEIS.pelajar_by_kelas.filter((e) => {
+    return e['ID Institusi'] === inid;
+  });
+
+  if (pelajarByKelas.length <= 0) {
+    return res.status(404).json({ msg: 'No pelajar found' });
+  }
+
+  res.status(200).json(pelajarByKelas);
 };
 
 export { getSekolahMOEIS, getPelajarMOEIS };
