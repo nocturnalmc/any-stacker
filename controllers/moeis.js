3091af00fc30bceec802597d0737c2173b1f3dde
noct <ezy_95@outlook.com>
2023-03-28 16:58:33 +0800

    feat: two way MOEIS integration implemented, one for real, another for local

--- controllers/moeis.js
+++ controllers/moeis.js
@@ -1,32 +1,90 @@
+import axios from 'axios';
+import https from 'https';
 import sekolahMOEIS from '../data/sekolah.json' assert { type: 'json' };
 import pelajarMOEIS from '../data/pelajar.json' assert { type: 'json' };
 
+// GET /sekolah
 const getSekolahMOEIS = async (req, res) => {
-  const { jkod, katkod } = req.query;
+  const { jkod, katkod, pkod, opskod, stpm, jnskod, special } = req.query;
 
-  const sekolahByNegeri = sekolahMOEIS.sekolah_by_negeri.filter((e) => {
-    return e.NEGERI === jkod;
-  });
+  if (process.env.MOEIS_APIKEY) {
+    console.log('query MOEIS sekolah');
+    try {
+      const agent = new https.Agent({
+        rejectUnauthorized: false,
+      });
+      const { data } = await axios.get(
+        process.env.MOEIS_INTEGRATION_URL_SEKOLAH +
+          `?jkod=${jkod}
+          ${katkod ? `&katkod=${katkod}` : ''}${pkod ? `&pkod=${pkod}` : ''}${
+            opskod ? `&opskod=${opskod}` : ''
+          }${stpm ? `&stpm=${stpm}` : ''}${jnskod ? `&jnskod=${jnskod}` : ''}${
+            special ? `&special=${special}` : ''
+          }`,
+        {
+          httpsAgent: agent,
+          headers: {
+            APIKEY: process.env.MOEIS_APIKEY,
+          },
+        }
+      );
+      return res.status(200).json(data);
+    } catch (error) {
+      return res.json({ msg: error });
+    }
+  } else {
+    console.log('query local data sekolah');
+    const sekolahByNegeri = sekolahMOEIS.sekolah_by_negeri.filter((e) => {
+      return e.NEGERI === jkod;
+    });
 
-  if (sekolahByNegeri.length <= 0) {
-    return res.status(404).json({ msg: 'No sekolah found' });
+    if (sekolahByNegeri.length <= 0) {
+      return res.status(404).json({ msg: 'No sekolah found' });
+    }
+
+    res.status(200).json(sekolahByNegeri);
   }
-
-  res.status(200).json(sekolahByNegeri);
 };
 
+// GET /pelajar
 const getPelajarMOEIS = async (req, res) => {
-  const { inid, pkid } = req.query;
+  const { inid, pkid, statusoku } = req.query;
 
-  const pelajarByKelas = pelajarMOEIS.pelajar_by_kelas.filter((e) => {
-    return e['ID Institusi'] === inid;
-  });
+  if (process.env.MOEIS_APIKEY) {
+    console.log('query MOEIS pelajar');
+    try {
+      const agent = new https.Agent({
+        rejectUnauthorized: false,
+      });
+      const { data } = await axios.get(
+        process.env.MOEIS_INTEGRATION_URL_PELAJAR +
+          `?inid=${inid}
+        ${pkid ? `&pkid=${pkid}` : ''}${
+            statusoku ? `&statusoku=${statusoku}` : ''
+          }`,
+        {
+          httpsAgent: agent,
+          headers: {
+            APIKEY: process.env.MOEIS_APIKEY,
+          },
+        }
+      );
+      return res.status(200).json(data);
+    } catch (error) {
+      return res.json({ msg: error });
+    }
+  } else {
+    console.log('query local data pelajar');
+    const pelajarByKelas = pelajarMOEIS.pelajar_by_kelas.filter((e) => {
+      return e['ID Institusi'] === inid;
+    });
 
-  if (pelajarByKelas.length <= 0) {
-    return res.status(404).json({ msg: 'No pelajar found' });
+    if (pelajarByKelas.length <= 0) {
+      return res.status(404).json({ msg: 'No pelajar found' });
+    }
+
+    res.status(200).json(pelajarByKelas);
   }
-
-  res.status(200).json(pelajarByKelas);
 };
 
 export { getSekolahMOEIS, getPelajarMOEIS };
--- middlewares/moeisAuth.js
+++ middlewares/moeisAuth.js
@@ -1,9 +1,9 @@
 const moeisAuth = async (req, res, next) => {
   const apikey = req.headers.apikey;
-  if (!apikey || apikey !== process.env.MOEIS_APIKEY) {
+  if (!apikey || apikey !== process.env.OWN_MIDDLEWARE_MOEIS_APIKEY) {
     return res.status(404).json({ msg: 'Unauthorized' });
   }
-  if (apikey === process.env.MOEIS_APIKEY) {
+  if (apikey === process.env.OWN_MIDDLEWARE_MOEIS_APIKEY) {
     next();
   }
 };
